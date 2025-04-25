24f715f31c0585d92ca3fb5871153e53de5e55c9
vitoretto <duartevoliveira@gmail.com>
2025-04-24 23:04:41 -0300

    vhod0 - criando dir para scripts de treino

---
+++ desenferrujando/hello.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# +----------------------------------------------+
+# |                                              |
+# |      Don Vito's Official BASH Script™️        |
+# |                                              |
+# |    "Respect the script. Follow the code."    |
+# |                                              |
+# |              Est. 2025                       |
+# +----------------------------------------------+
+
+whom="cuzaum"
+printf "Salve, %s\n" "$whom"
+
+printf "Salve, %s\n" "$1"
---
+++ desenferrujando/ola_mundo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# +----------------------------------------------+
+# |                                              |
+# |      Don Vito's Official BASH Script         |
+# |                                              |
+# |     "I’ll make you a script you can’t        |
+# |                  refuse"                     |
+# |                                              |
+# |                 Est. 2025                    |
+# +----------------------------------------------+
+
+echo "Who da fuck are you?"
+read name
+echo "Sup, $name?!"
+
