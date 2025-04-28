b7537f847ea87de4fe76475c64df663e41339e8b
vitoretto <duartevoliveira@gmail.com>
2025-04-28 16:22:09 -0300

    vhod1 - criando script readlink

---
+++ desenferrujando/readlink.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# +----------------------------------------------+
+# |                                              |
+# |      Don Vito's Official BASH Script         |
+# |                                              |
+# |     "I’ll make you a script you can’t        |
+# |                  refuse"                     |
+# |                                              |
+# |                 Est. 2025                    |
+# +----------------------------------------------+
+
+cd "$(dirname "$(readlink -f "$0")")"
+echo "Você está no diretório $PWD"
