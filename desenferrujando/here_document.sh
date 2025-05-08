fb1bb87ddca05d3462e89d521a083aa1c27cd27e
vitoretto <duartevoliveira@gmail.com>
2025-05-07 21:40:48 -0300

    vhod2 - criando o here_document script

---
+++ desenferrujando/here_document.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# +----------------------------------------------+
+# |                                              |
+# |      Don Vito's Official BASH Script™️        |
+# |                                              |
+# |    "Respect the script. Follow the code."    |
+# |                                              |
+# |              Est. 2025                       |
+# +----------------------------------------------+
+
+cat <<END >file
+Hello, we're here: "$PWD"
+END
+
