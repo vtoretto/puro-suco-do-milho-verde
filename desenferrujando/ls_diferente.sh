f4178e9e431bda4044b488dafde38a6d6510f0db
vitoretto <duartevoliveira@gmail.com>
2025-05-07 21:48:08 -0300

    criando o script ls_diferente

---
+++ desenferrujando/ls_diferente.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# +----------------------------------------------+
+# |                                              |
+# |      Don Vito's Official BASH Script™️        |
+# |                                              |
+# |    "Respect the script. Follow the code."    |
+# |                                              |
+# |              Est. 2025                       |
+# +----------------------------------------------+
+
+# lista arquivos com extensões .{md,yaml}
+printf "%s\n" *.{md,yaml}
